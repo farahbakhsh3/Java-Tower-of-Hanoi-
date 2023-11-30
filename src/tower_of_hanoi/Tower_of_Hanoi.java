abf3d0508feb6a7ab9af1cd367aae9f4df446ed1
Amiin Farahbakhsh <farahbakhsh3@gmail.com>
2023-11-30 13:01:17 +0330

    Add Scanner and a bit of optimization. 😁

--- src/tower_of_hanoi/Tower_of_Hanoi.java
+++ src/tower_of_hanoi/Tower_of_Hanoi.java
@@ -1,19 +1,35 @@
 package tower_of_hanoi;
 
+import java.util.Scanner;
+
 public class Tower_of_Hanoi {
 
+    static int i = 1;
+    
     static void towerOfHanoi(int n, char source, char helper, char target) {
         if (n == 1) {
-            System.out.println("Take disk 1 from " + source + " to " + target);
+            System.out.format("%03d : Take disk 1 from %c to %c\n", i, source, target);
+            i++;
             return;
         }
+
         towerOfHanoi(n - 1, source, target, helper);
-        System.out.println("Take disk " + n + " from " + source + " to " + target);
+        System.out.format("%03d : Take disk %d from %c to %c\n", i, n, source, target);
+        i++;
         towerOfHanoi(n - 1, helper, source, target);
     }
 
     public static void main(String args[]) {
-        int n = 3;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Input n : ");
+        int n = input.nextInt();
+        
+        if (n <= 0){
+            System.err.println("Wrong n.");
+            return;
+        }
+            
         towerOfHanoi(n, 'A', 'B', 'C');
     }
 }
